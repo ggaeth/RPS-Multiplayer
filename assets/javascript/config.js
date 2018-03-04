79ac139dd0bc272e29f47b148f5ee2ee07b7b324
ggaeth <34075514+ggaeth@users.noreply.github.com>
2018-03-03 22:08:19 -0600

    RPS

---
+++ assets/javascript/config.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBep8Z-jirWcPOg94Y7VQnNhM6goMEDuDs",
+    authDomain: "fir-rps-223c3.firebaseapp.com",
+    databaseURL: "https://fir-rps-223c3.firebaseio.com",
+    storageBucket: "fir-rps-223c3.appspot.com",
+    messagingSenderId: "1069556453649"
+};
+firebase.initializeApp(config);
+
+
